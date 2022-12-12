5327ef6f7486974dffacc6b6b474acddfc9978cf
guillemfrancisco <gfrancisco@obsa.ad>
2022-12-12 10:11:59 +0000

    fix: returning appropiate status code

--- fastapi_jwt_auth/auth_jwt.py
+++ fastapi_jwt_auth/auth_jwt.py
@@ -690,7 +690,7 @@ class AuthJWT(AuthConfig):
         if len(self._required_roles)>0:
             for role in self._required_roles:
                 if role not in token_roles:
-                    raise NotEnoughPermissions(status_code=401, message="Not enough permissions")
+                    raise NotEnoughPermissions(status_code=403, message="Not enough permissions")
     
     def _verifying_claims(self, raw_token: dict) -> None:
         if len(self._required_claims) > 0:
